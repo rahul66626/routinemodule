34d66005d17384d153e6c54375ed9e923669a3b1
Rahul Jacob <e.rahuljacob@gmail.com>
2021-07-18 02:54:40 +0530

    first commit

---
+++ src/main/java/com/xyz/sam/routinemodule/RoutineModuleApplication.java
@@ -0,0 +1,13 @@
+package com.xyz.sam.routinemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutineModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xyz/sam/routinemodule/RoutineModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.sam.routinemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
